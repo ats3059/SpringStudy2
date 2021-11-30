88ef81848285412c04ae6a63caea77f0cbc65178
taseon <ats3059@naver.com>
2021-11-30 20:28:15 +0900

    Application Context Bean Check

--- src/main/java/hello/servlet/domain/member/Member2.java
+++ src/main/java/hello/servlet/domain/member/Member2.java
@@ -1,53 +1,56 @@
 package hello.servlet.domain.member;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class Member2 {
 
     private Long id;
     private String username;
     private int age;
-
-    // 빌더 소환: 외부에서 Car.builder() 형태로 접근 가능하게 스태틱 메소드로
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    private Member2(Builder builder){
-        this.id = builder.id;
-        this.username = builder.username;
-        this.age = builder.age;
-    }
-
-
-    public static class Builder{
-        private Long id;
-        private String username;
-        private int age;
-
-        public Builder setId(Long value){
-            this.id = value;
-            return this;
-        }
-
-        public Builder setUserName(String value){
-            this.username = value;
-            return this;
-        }
-
-        public Builder setAge(int age){
-            this.age = age;
-            return this;
-        }
-
-        public Member2 build(){
-            return new Member2(this);
-        }
-
-    }
+//
+//    // 빌더 소환: 외부에서 Car.builder() 형태로 접근 가능하게 스태틱 메소드로
+//    public static Builder builder() {
+//        return new Builder();
+//    }
+//
+//    private Member2(Builder builder){
+//        this.id = builder.id;
+//        this.username = builder.username;
+//        this.age = builder.age;
+//    }
+//
+//
+//    public static class Builder{
+//        private Long id;
+//        private String username;
+//        private int age;
+//
+//        public Builder setId(Long value){
+//            this.id = value;
+//            return this;
+//        }
+//
+//        public Builder setUserName(String value){
+//            this.username = value;
+//            return this;
+//        }
+//
+//        public Builder setAge(int age){
+//            this.age = age;
+//            return this;
+//        }
+//
+//        public Member2 build(){
+//            return new Member2(this);
+//        }
+//
+//    }
 
 
 
--- src/test/java/hello/servlet/domain/member/Test2.java
+++ src/test/java/hello/servlet/domain/member/Test2.java
@@ -1,15 +1,28 @@
 package hello.servlet.domain.member;
 
+import hello.servlet.ServletApplication;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
 
 public class Test2 {
 
     @Test
     void builderTest(){
-        Member2 member2 = Member2.builder()
-                                .setAge(10)
-                                .setId(10L)
-                                .setUserName("25").build();
+
+
+        Member2 member2 = Member2.builder().age(10).id(12L).username("123").build();
+        Member2 member21 = new Member2(12L,"123",123);
+
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ServletApplication.class);
+
+        Arrays.stream(ac.getBeanDefinitionNames()).forEach((value) -> System.out.println("value = " + value));
+
+
+
 
         System.out.println("member2.getUsername() = " + member2.getUsername());
 
